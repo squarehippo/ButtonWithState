fe944b2b4d5561538684198e72078258c8110ac5
Brian Wilson <squarehippobills@gmail.com>
2021-06-11 15:23:05 -0400

    Initial Commit

---
+++ ButtonWithState/ButtonWithStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonWithStateApp.swift
+//  ButtonWithState
+//
+//  Created by Brian Wilson on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ButtonWithStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonWithState/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ButtonWithState
+//
+//  Created by Brian Wilson on 6/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
