3a19112da740a40bc489aaece6304585ab5c784a
Brian Wilson <squarehippobills@gmail.com>
2021-06-11 15:49:00 -0400

    Complete example

--- ButtonWithState/ContentView.swift
+++ ButtonWithState/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rememberMe = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            PushButton(title: "Remember", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
+    }
+}
+
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.8), Color(white: 0.5)]
+    
+    var body: some View {
+        Button(title) {
+            self.isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
     }
 }
 
